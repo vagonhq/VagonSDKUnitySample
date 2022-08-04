e2a080f49abc15ba77a2dfe6b71325d6ae3c9c09
Arda Cinar <kuzux92@gmail.com>
2022-08-04 12:59:48 +0000

    Initial commit

---
+++ Assets/VagonSample.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using WebSocketSharp;
+
+public class VagonSample : MonoBehaviour
+{
+    WebSocket ws;
+    bool shouldChange = false;
+    int msgCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ws = new WebSocket("ws://localhost:7788");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Got " + e.Data);
+            msgCount++;
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var obj = GameObject.Find("label");
+        var tmptext = obj.GetComponent<TextMeshProUGUI>();
+        tmptext.text = $"Got {msgCount} messages";
+    }
+}
